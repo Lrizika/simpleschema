df7d3f7fc0b76909226d03992c79b5aac6bdb555
larkeith <llarkeith@gmail.com>
2022-11-02 17:00:45 -0700

    Create ObjectSchema helper class Used for indicating when a schema is for an object Rather than a dict

--- simpleschema/__init__.py
+++ simpleschema/__init__.py
@@ -1,3 +1,4 @@
 
-from simpleschema.validate import validateSchema as validate
+from simpleschema.validate import validateSchema as validate, validateItem
 from simpleschema.validate import is_valid, logger
+from simpleschema.helper_classes import ObjectSchema
---
+++ simpleschema/helper_classes.py
@@ -0,0 +1,10 @@
+
+
+class ObjectSchema(dict):
+	"""
+	A schema for an object, rather than a dictionary
+	ObjectSchema are identical to dicts
+	They are used to indicate a schema should be applied to an object
+	"""
+
+	pass
