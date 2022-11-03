5cac91e32ddfc94fb7bc228120af11f32a6d86a3
larkeith <llarkeith@gmail.com>
2022-11-02 17:21:57 -0700

    Improved ObjectSchema example

--- simpleschema/helper_classes.py
+++ simpleschema/helper_classes.py
@@ -7,33 +7,38 @@ class ObjectSchema(dict):
 	They are used to indicate a schema should be applied to an object
 
 	Example Usage:
-		>>> my_schema = simpleschema.ObjectSchema({
-		...     'required_method': callable,
-		...     'required_string_attribute': str,
-		...     ('required_a_or', 'required_b'): object,
-		... })
-		>>> class ValidClass:
-		...     required_string_attribute = 'string'
-		...     def required_method():
-		...             pass
-		...     def required_b(self):
-		...             pass
-		>>> class InvalidClass:
-		...     required_string_attribute = 123
-		...     def required_method():
-		...             pass
-		...     def required_b(self):
-		...             pass
-		>>> simpleschema.validate(ValidClass, my_schema)
-		True
-		>>> simpleschema.validate(InvalidClass, my_schema)
-		Traceback (most recent call last):
-		File "<stdin>", line 1, in <module>
-		File "/var/git-shared/simpleschema/simpleschema/validate.py", line 61, in validateSchema
-			validateItem(item[schema_key], schema_val)
-		File "/var/git-shared/simpleschema/simpleschema/validate.py", line 108, in validateItem
-			raise ValueError(f'Schema constraint `{schema_val}`, item `{item_val}` - Type requirement mismatch')
-		ValueError: Schema constraint `<class 'str'>`, item `123` - Type requirement mismatch
+
+my_schema = simpleschema.ObjectSchema({
+	'required_method': callable,
+	'required_string_attribute': str,
+	('required_a_or', 'required_b'): object,
+})
+class ValidClass:
+	required_string_attribute = 'string'
+	def required_method():
+		pass
+	def required_b(self):
+		pass
+class ValidOnlyIfInstantiated:
+	def __init__(self):
+		self.required_string_attribute = 'different string'
+	def required_method():
+		pass
+	def required_b(self):
+		pass
+class InvalidClass:
+	required_string_attribute = 123
+	def required_method():
+		pass
+	def required_b(self):
+		pass
+simpleschema.validate(ValidClass, my_schema)  # True
+simpleschema.validate(ValidClass(), my_schema)  # True
+simpleschema.validate(ValidOnlyIfInstantiated, my_schema)  # ValueError
+simpleschema.validate(ValidOnlyIfInstantiated(), my_schema)  # True
+simpleschema.validate(InvalidClass, my_schema)  # ValueError
+simpleschema.validate(InvalidClass(), my_schema)  # ValueError
+
 	"""
 
 	pass
