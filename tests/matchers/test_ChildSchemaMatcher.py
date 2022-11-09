22a598c02eff6664eef77ba36d254a207bc24c96
larkeith <llarkeith@gmail.com>
2022-11-08 20:46:19 -0800

    Add more ChildSchemaMatcher tests

--- tests/matchers/test_ChildSchemaMatcher.py
+++ tests/matchers/test_ChildSchemaMatcher.py
@@ -3,6 +3,7 @@ import typing
 import re
 from simpleschema.matchers import ChildSchemaMatcher
 from simpleschema.exceptions import SchemaValidationFailure
+import simpleschema
 from tests.matchers import matcher_test_framework
 
 
@@ -13,9 +14,11 @@ class TestChildSchemaMatcher(matcher_test_framework.TestMatcher):
 	raises = SchemaValidationFailure
 	valid_pairs = [
 		({str: int}, {'asdf': 1234}),
+		({str: simpleschema.Literal(int)}, {'asdf': int}),
 	]
 	invalid_pairs = [
 		({str: int}, {1234: 1234}),
+		({str: simpleschema.Literal(int)}, {'asdf': 1234}),
 	]
 	inapplicable_constraints = ['asdf', typing.Any, typing.Iterable, r'.*', None, re.compile('asdf')]
 
